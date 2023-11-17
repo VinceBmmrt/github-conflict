6b9d27099e100d80bb4d5f5cbf80bf981e7a7dc2
Aspitrine <loquente62@gmail.com>
2023-11-17 16:14:11 +0100

    Oups

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,6 +1,9 @@
 import { configureStore } from '@reduxjs/toolkit';
+import recipiesReducer from './reducers/recipes';
 
 // eslint-disable-next-line import/prefer-default-export
 export const store = configureStore({
-  reducer: {},
+  reducer: {
+    recipes: recipiesReducer,
+  },
 });
